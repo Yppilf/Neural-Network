2eaacdbcb30d767a2606ba9e0efd790a8f0e1179
Yppilf <thomas.smeman@gmail.com>
2024-08-27 21:26:38 +0200

    swapped to csv for loading data

--- CPPversion/dataGetter/load_files.cc
+++ CPPversion/dataGetter/load_files.cc
@@ -1,40 +1,48 @@
 #include "dataGetter.ih"
 
 // Helper function to read binary data into 3D vector
-vector<vector<vector<double>>> read_3d_array_from_file(const string& file_name)
+vector<vector<vector<double>>> read_3d_array_from_csv(const string& filename)
 {
     vector<vector<vector<double>>> array;
+    string line;
+    vector<vector<double>> slice;
 
-    // Open file in binary mode
-    ifstream file(file_name, ios::binary);
-    if (!file)
-    {
-        return array;   // Return empty array on failure
+    ifstream file(filename);
+    if (!file) {
+        return array;   // Return empty array
     }
 
-    // Read dimensions of 3D array
-    size_t n1, n2, n3;
-    file.read(reinterpret_cast<char*>(&n1), sizeof(size_t));
-    file.read(reinterpret_cast<char*>(&n2), sizeof(size_t));
-    file.read(reinterpret_cast<char*>(&n3), sizeof(size_t));
-    array.resize(n1, vector<vector<double>>(n2, vector<double>(n3)));
-
-    // Read data 
-    for (size_t i = 0; i != n1; ++i) {
-        for (size_t j = 0; j != n2; ++j) {
-            file.read(reinterpret_cast<char*>(array[i][j].data()), n3*sizeof(double));
+    while(getline(file,line)) {
+        if (line.empty()) {
+            if (!slice.empty()) {
+                array.push_back(slice);
+                slice.clear();
+            }
+            continue;
         }
+
+        vector<double> row;
+        stringstream ss(line);
+        string item;
+
+        while (getline(ss, item, ',')) {
+            row.push_back(stod(item));
+        }
+
+        slice.push_back(row);
+    }
+    
+    if (!slice.empty()) {
+        array.push_back(slice);
     }
 
-    // Cleanup
-    file.close();
     return array;
 }
 
 void DataGetter::load_files(const string& folder_name)
 {
-    x_train = read_3d_array_from_file(folder_name + "/training_data.npy");
-    y_train = read_3d_array_from_file(folder_name + "/training_labels.npy");
-    x_test = read_3d_array_from_file(folder_name + "/testing_data.npy");
-    y_test = read_3d_array_from_file(folder_name + "/testing_labels.npy");
+    x_train = read_3d_array_from_csv(folder_name + "/training_data.npy");
+    y_train = read_3d_array_from_csv(folder_name + "/training_labels.npy");
+    x_test = read_3d_array_from_csv(folder_name + "/testing_data.npy");
+    y_test = read_3d_array_from_csv(folder_name + "/testing_labels.npy");
 }
--- CPPversion/main.cc
+++ CPPversion/main.cc
@@ -4,11 +4,13 @@ int main()
 {
     // Acquire data
     DataGetter dataGetter;
-    dataGetter.load_files("mini_mnist_data");
+    dataGetter.load_files("../mini_mnist_data_2");
     auto x_train = dataGetter.getXTrain();
     auto y_train = dataGetter.getYTrain();
     auto x_test = dataGetter.getXTest();
     auto y_test = dataGetter.getYTest();
+
+    cout << y_test[0][0][0] << "\n";
 
     // Create network
 
