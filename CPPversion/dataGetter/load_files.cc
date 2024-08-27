a36da1e6440c34bff29caaa6447baa0a2f77a74e
Yppilf <thomas.smeman@gmail.com>
2024-08-27 21:52:44 +0200

    got data loaded

--- CPPversion/dataGetter/load_files.cc
+++ CPPversion/dataGetter/load_files.cc
@@ -6,32 +6,44 @@ vector<vector<vector<double>>> read_3d_array_from_csv(const string& filename)
     vector<vector<vector<double>>> array;
     string line;
     vector<vector<double>> slice;
+    vector<double> row;
 
     ifstream file(filename);
     if (!file) {
+        cerr << "Couldnt open file" << filename << "\n";
         return array;   // Return empty array
     }
 
-    while(getline(file,line)) {
+    while (getline(file, line)) {
+        line.erase(0, line.find_first_not_of(" \t\n\r\f\v"));
+        line.erase(line.find_last_not_of(" \t\n\r\f\v") + 1);
+
         if (line.empty()) {
-            if (!slice.empty()) {
-                array.push_back(slice);
-                slice.clear();
+            if (!row.empty() || !slice.empty()) {
+                if (!row.empty()) {
+                    slice.push_back(row);
+                    row.clear();
+                }
+                if (!slice.empty()) {
+                    array.push_back(slice);
+                    slice.clear();
+                }
             }
-            continue;
+        } else {
+            stringstream ss(line);
+            string item;
+            while (getline(ss, item, ',')) {
+                row.push_back(stod(item));
+            }
+            if (!row.empty()) {
+                slice.push_back(row);
+            }
         }
+    }
 
-        vector<double> row;
-        stringstream ss(line);
-        string item;
-
-        while (getline(ss, item, ',')) {
-            row.push_back(stod(item));
-        }
-
+    if (!row.empty()) {
         slice.push_back(row);
     }
-    
     if (!slice.empty()) {
         array.push_back(slice);
     }
@@ -41,8 +53,8 @@ vector<vector<vector<double>>> read_3d_array_from_csv(const string& filename)
 
 void DataGetter::load_files(const string& folder_name)
 {
-    x_train = read_3d_array_from_csv(folder_name + "/training_data.npy");
-    y_train = read_3d_array_from_csv(folder_name + "/training_labels.npy");
-    x_test = read_3d_array_from_csv(folder_name + "/testing_data.npy");
-    y_test = read_3d_array_from_csv(folder_name + "/testing_labels.npy");
+    x_train = read_3d_array_from_csv(folder_name + "/x_train.csv");
+    y_train = read_3d_array_from_csv(folder_name + "/y_train.csv");
+    x_test = read_3d_array_from_csv(folder_name + "/x_test.csv");
+    y_test = read_3d_array_from_csv(folder_name + "/y_test.csv");
 }
--- CPPversion/main.cc
+++ CPPversion/main.cc
@@ -1,5 +1,19 @@
 #include "main.ih"
 
+void print_3d_vector(const vector<vector<vector<double>>>& vec) 
+{
+    for (size_t i = 0; i < vec.size(); ++i) {
+        cout << "Slice " << i << ":\n";
+        for (size_t j = 0; j < vec[i].size(); ++j) {
+            for (size_t k = 0; k < vec[i][j].size(); ++k) {
+                cout << vec[i][j][k] << " ";
+            }
+            cout << "\n";
+        }
+        cout << "\n";
+    }
+}
+
 int main()
 {
     // Acquire data
@@ -9,8 +23,6 @@ int main()
     auto y_train = dataGetter.getYTrain();
     auto x_test = dataGetter.getXTest();
     auto y_test = dataGetter.getYTest();
-
-    cout << y_test[0][0][0] << "\n";
 
     // Create network
 
