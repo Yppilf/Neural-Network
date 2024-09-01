f32d582a068d50859033609bcc26993bdf5ed7ed
Yppilf <109002969+Yppilf@users.noreply.github.com>
2024-09-01 16:19:16 +0200

    updated readme

--- CPPversion/main.cc
+++ CPPversion/main.cc
@@ -2,7 +2,6 @@
 
 int main()
 {
-    cout << "1";
     // Acquire data
     DataGetter dataGetter;
     dataGetter.load_files("../mnist_data_2");
@@ -11,12 +10,10 @@ int main()
     auto x_test = dataGetter.getXTest();
     auto y_test = dataGetter.getYTest();
 
-    cout << "2";
     // Transpose data where needed
     y_train = dataGetter.transposeLabels(y_train);
     y_test = dataGetter.transposeLabels(y_test);
 
-    cout << "3";
     // Create network
     vector<shared_ptr<Layer>> networkStructure = {
         make_shared<Dense>(28*28, 40),
@@ -26,11 +23,9 @@ int main()
     };
     Network network(networkStructure, 0.1);
 
-    cout << "4";
     // Train network
     vector<double> errors = network.train(mse, mse_prime, x_train, y_train, 50, true);
 
-    cout << "5";
     // Save network
     network.saveNetwork("mnist3.json");
 }
--- README.md
+++ README.md
@@ -8,3 +8,8 @@ Fixed MNIST imports
 Converted to object oriented approach
 Added save and load functionality
 CNN code does not work currently, only the neural network does. See mnist.py for example codes. Use mnist.json for a model on the MNIST database
+
+# C++  version
+In the folder "CPPversion", a lot of the functionality of the python module has been written in C++ for faster execution. 
+The code does not require any external modules outside of the default and should function by simply running "make" in the directory on a linux machine (otherwise untested)
+Strangely in the current version the code works fine for the mini_mnist_data, but immediately throws a segmentation fault for larger folders, without changing any other code. I do not know why this happens.
